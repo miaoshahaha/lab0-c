f9bb9aeae43bdf0b6b61f909164f1f430b194ff5
haha <wonmorgan02@gmail.com>
2025-03-05 15:41:26 +0800

    Implement the q_size
    After adding t the q_size, the result is constant.
    
    Change-Id: I1c4003d51b660ef4070619b097e9ef5feb654649

--- queue.c
+++ queue.c
@@ -95,7 +95,16 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+
+    struct list_head *next;
+    int cnt = 0;
+
+    list_for_each (next, head) {
+        cnt++;
+    }
+    return cnt;
 }
 
 /* Delete the middle node in queue */
